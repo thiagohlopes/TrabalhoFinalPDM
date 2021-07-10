fff5cadb71257060fadf7bd0c55e83c8774700ba
Eduardo Garcia <eduardogarcia79@live.com>
2021-07-10 20:14:51 -0300

    Nav Bar E Fragments

--- app/src/main/java/com/example/trabalhofinal/ui/CartFragment.java
+++ app/src/main/java/com/example/trabalhofinal/ui/CartFragment.java
@@ -1,6 +1,33 @@
-package com.example.trabalhofinal;
+package com.example.trabalhofinal.ui;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.trabalhofinal.R;
+
 public class CartFragment extends Fragment {
+    public static CartFragment newInstance() {
+        return new CartFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_cart, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+
 }
